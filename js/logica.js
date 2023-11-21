338d27af103d5b77d0b0dcce450d71d3c33a4d3d
Edson Moreno <diego2816@gmail.com>
2023-11-21 12:29:45 -0400

    Creando logica de elejir al campeon 1

--- js/logica.js
+++ js/logica.js
@@ -1,1 +1,20 @@
-alert("Bienvenido");
+function Campeon() {
+    let c_elejido;
+    if(document.getElementById("misu").checked){
+        c_elejido = "Misu";
+    }else if(document.getElementById("tsuchi").checked){
+        c_elejido = "Tsuchi";
+    }else if(document.getElementById("hi").checked){
+        c_elejido = "Hi";
+    }else if(document.getElementById("kaze").checked){
+        c_elejido = "Kaze";
+    }
+    alert("Elejiste: "+c_elejido);
+}
+//alert("Bienvenido");
+window.onload = function(){
+    let campeon = document.getElementById("campeon");
+    campeon.addEventListener("click",Campeon);
+
+}
+
